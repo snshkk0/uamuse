0163660b73d78e27f0916c3f9378f8d40c33a66b
snshkk0 <okenneko393@gmail.com>
2026-05-10 15:55:55 +0300

    new styles

---
+++ script.js
@@ -0,0 +1,58 @@
+const modal = document.getElementById("spotify_modal");
+const playerContainer = document.getElementById("spotify_player_container");
+const closeBtn = document.querySelector(".close_button");
+
+
+document.querySelectorAll('.genre_button').forEach(button => {
+    button.addEventListener('click', () => {
+        const spotifyId = button.getAttribute('data_playlist');
+        
+        // Створюємо код айфрейму
+        playerContainer.innerHTML = `
+            <iframe src="https://open.spotify.com/embed/playlist/${spotifyId}?utm_source=generator&theme=0" 
+                    width="100%" height="500" frameBorder="0" allowfullscreen="" 
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                    loading="lazy"></iframe>`;
+        
+        modal.style.display = "block"; 
+    });
+});
+
+
+closeBtn.onclick = () => {
+    modal.style.display = "none";
+    playerContainer.innerHTML = ""; 
+};
+
+
+let footerClicks = 0;
+
+const footerTag = document.querySelector('footer');
+
+if (footerTag) {
+    footerTag.addEventListener('click', () => {
+        footerClicks++;
+        
+   
+        if (footerClicks === 5) {
+            activatePartyMode();
+            footerClicks = 0; 
+        }
+    });
+}
+
+function activatePartyMode() {
+    const logo = document.querySelector('h1') || document.querySelector('.uamuse-title');
+    
+    if (logo) {
+      
+        logo.classList.add('dancing_logo');
+        
+        console.log("Party mode activated!");
+
+        setTimeout(() => {
+            logo.classList.remove('dancing_logo');
+        }, 5000);
+    }
+}
+
